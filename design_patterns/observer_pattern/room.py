eaafef32e070bdf14ba4e05dc808157b9dc66adb
kBasahr <khyrul.bashar@gigalogy.com>
2024-09-26 17:32:52 +0600

    flake8 resolved

--- design_patterns/observer_pattern/room.py
+++ design_patterns/observer_pattern/room.py
@@ -1,24 +1,24 @@
 from subject import Subject
 from observer import Observer
 
+
 class Room(Subject):
 
     def __init__(self):
         self._observers = list()
-        
+
         self._number_of_persons = 0
         print("Initiated an empty room!")
-        
+
     def addObserver(self, observer: Observer):
         self._observers.append(observer)
-    
+
     def removeObserver(self, observer: Observer):
         self._observers.remove(observer)
 
     def notifyObservers(self):
         for observer in self._observers:
             observer.update()
-    
 
     # class specific methods
 
@@ -27,6 +27,6 @@ class Room(Subject):
 
         # state has changed lets notify the observers
         self.notifyObservers()
-    
+
     def getPerson(self):
         return self._number_of_persons
--- design_patterns/observer_pattern/subject.py
+++ design_patterns/observer_pattern/subject.py
@@ -2,12 +2,13 @@ from abc import ABC, abstractmethod
 
 from observer import Observer
 
+
 class Subject(ABC):
-    
+
     @abstractmethod
     def addObserver(self, observer: Observer):
         pass
-    
+
     @abstractmethod
     def removeObserver(self, observer: Observer):
         pass
